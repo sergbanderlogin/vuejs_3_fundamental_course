ceeb9b50a9c149166b5dbbdf9201ad3c745661be
sergbanderlogin <sergbanderlogin@gmail.com>
2022-07-14 19:07:24 +0300

    introduction vuejs-3

---
+++ 01_vue_3_introduction/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>Працює!</div>
+  <div>
+    <button @click="addLike">Like</button>
+    <button @click="addDisLikes">DisLike</button>
+  </div>
+  <div>Кількість вподобайок <strong>{{ likes }}</strong></div>
+  <div>Кількість дизлайків <strong>{{ dislikes }}</strong></div>
+</template>
+
+<script>
+export default {
+  name: "App.vue",
+  data() {
+    return {
+      likes: 0,
+      dislikes: 0,
+    }
+  },
+  methods: {
+    addLike() {
+      this.likes += 1
+    },
+    addDisLikes() {
+      this.dislikes += 1
+    },
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
